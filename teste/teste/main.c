92bf1a26650e2dfd021cec6cd771e109603c75ae
sandrospsantos <sandrobh_bja@yahoo.com.br>
2020-03-21 21:16:46 -0300

    Commit Inicial de Teste

---
+++ teste/teste/main.c
@@ -0,0 +1,18 @@
+/*
+ * teste.c
+ *
+ * Created: 3/21/2020 9:16:14 PM
+ * Author : Win 10
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
